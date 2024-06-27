25a0a149aa8cc3e771a1b2c13aeb3a2919ca6316
Mtseng <mtseng36@gmail.com>
2024-06-27 15:15:00 +0500

    added КриптоПРО install

--- Bash script/setup.sh
+++ Bash script/setup.sh
@@ -1,6 +1,11 @@
 
 #!/bin/bash
 
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Error: this script must be run as root or via sudo" >&2
+    exit 1
+fi
+
 cd /root/Bash\ script
 
 $iStatus
@@ -14,8 +19,8 @@ fi
 
 if [[ -z "$iStatus" && -n "$paksFolder" ]]; then
 paksList="`ls ./paks`"
-paksCheckOffline="`echo $paksList | grep '1c-paks.tar.gz' | grep 'cabextract-1.9.1-3.red80.x86_64.rpm' | grep 'enchant-1.6.0-29.red80.x86_64.rpm'| grep 'kesl-11.3.0-7441.x86_64.rpm'| grep 'kesl-gui-11.3.0-7441.x86_64.rpm'| grep 'msttcore-fonts-installer-2.6-3.noarch.rpm'| grep 'perl-File-Copy-2.39-494.red80.noarch.rpm'| grep 'perl-Getopt-Long-2.52-494.red80.noarch.rpm'| grep 'xorg-x11-font-utils-7.5-53.red80.x86_64.rpm'`"
-paksCheckOnline="`echo $paksList | grep '1c-paks.tar.gz' | grep 'kesl-11.3.0-7441.x86_64.rpm'| grep 'kesl-gui-11.3.0-7441.x86_64.rpm'`"
+paksCheckOffline="`echo $paksList | grep '1c-paks.tar.gz' | grep 'cprocsp-paks.tar.gz' | grep 'cabextract-1.9.1-3.red80.x86_64.rpm' | grep 'enchant-1.6.0-29.red80.x86_64.rpm'| grep 'kesl-11.3.0-7441.x86_64.rpm'| grep 'kesl-gui-11.3.0-7441.x86_64.rpm'| grep 'msttcore-fonts-installer-2.6-3.noarch.rpm'| grep 'perl-File-Copy-2.39-494.red80.noarch.rpm'| grep 'perl-Getopt-Long-2.52-494.red80.noarch.rpm'| grep 'xorg-x11-font-utils-7.5-53.red80.x86_64.rpm'`"
+paksCheckOnline="`echo $paksList | grep '1c-paks.tar.gz' | grep 'cprocsp-paks.tar.gz' | grep 'kesl-11.3.0-7441.x86_64.rpm'| grep 'kesl-gui-11.3.0-7441.x86_64.rpm'`"
 fi
 
 echo 'Скрипт по установке ПО на RedOs'
@@ -80,6 +85,25 @@ if [[ -n "$iStatus" ]]; then
 fi
 
 dnf install kesl-gui-11.3.0-7441.x86_64.rpm -y
+
+echo "Установка КриптоПРО"
+
+tar -xzf cprocsp-paks.tar.gz
+
+cd cprocsp-paks
+
+dnf install lsb-cprocsp-base-5.0.12000-6.noarch.rpm lsb-cprocsp-rdr-64-5.0.12000-6.x86_64.rpm lsb-cprocsp-kc1-64-5.0.12000-6.x86_64.rpm lsb-cprocsp-capilite-64-5.0.12000-6.x86_64.rpm -y
+
+dnf install cprocsp-curl-64-5.0.12000-6.x86_64.rpm lsb-cprocsp-ca-certs-5.0.12000-6.noarch.rpm cprocsp-rdr-gui-gtk-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-pcsc-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-emv-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-inpaspot-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-kst-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-mskey-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-novacard-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-edoc-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-rutoken-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-cloud-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-cpfkc-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-infocrypt-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-rosan-64-5.0.12000-6.x86_64.rpm cprocsp-rdr-cryptoki-64-5.0.12000-6.x86_64.rpm cprocsp-cptools-gtk-64-5.0.12000-6.x86_64.rpm lsb-cprocsp-pkcs11-64-5.0.12000-6.x86_64.rpm lsb-cprocsp-import-ca-certs-5.0.12000-6.noarch.rpm -y
+
+cd /opt/cprocsp/lib/amd64
+
+libapi10="`ls | grep libcapi10 | tail -1`"
+libapi20="`ls | grep libcapi20 | tail -1`"
+
+gcc -shared -Wl,-soname,$libapi10,$libapi10 -o libcapilite.so
+
+
 
 
 
